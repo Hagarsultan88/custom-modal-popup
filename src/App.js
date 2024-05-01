96f8d84d3029cdf33e47f1014c4721a3d409fd91
Hagar Sultan <97818325+Hagarsultan88@users.noreply.github.com>
2024-05-01 09:43:42 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ModalTest from './Components/modal-test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ModalTest/>
+    </div>
+  );
+}
+
+export default App;
